4e2988baba38d9321c5a9374c70bdfb998affc26
dmcalpineuk <72901018+dmcalpineuk@users.noreply.github.com>
2024-09-04 19:17:26 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+__author__ = "Daniel McAlpine"
+__copyright__ = "Copyright 2024"
+__credits__ = "Junaid Haseeb, Vimal Kumar"
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Daniel McAlpine"
+__email__ = "contact@danielmcalpine.com"
+__status__ = "Development"
+__dependecies__ = "json"
+
+import json
+
+file_name = './cowrie.json'
+man_pages = './manpages.json'
+
+total_contents = []
+
+with open(file_name) as openfile:
+  for line in openfile:
+    total_contents.append(json.loads(line))
+
+with open(man_pages) as f:
+  man_pages_dict = json.load(f)
+
+for event in total_contents:
+  if 'cowrie.command.input' in event['eventid']:
+    try:
+      print(event['input'].split(" ", 1)[0], "-" , man_pages_dict[event['input'].split(" ", 1)[0]])
+    except Exception:
+      print(event['input'], "- unknown command")
