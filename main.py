aea8551c1e1746ffa59359e2fb38ee172e1b432b
dmcalpineuk <72901018+dmcalpineuk@users.noreply.github.com>
2024-09-04 21:11:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,18 +15,23 @@ import json
 file_name = './cowrie.json'
 man_pages = './manpages.json'
 
-total_contents = []
+def main():
 
-with open(file_name) as openfile:
-  for line in openfile:
-    total_contents.append(json.loads(line))
+  total_contents = []
 
-with open(man_pages) as f:
-  man_pages_dict = json.load(f)
+  with open(file_name) as openfile:
+    for line in openfile:
+      total_contents.append(json.loads(line))
 
-for event in total_contents:
-  if 'cowrie.command.input' in event['eventid']:
-    try:
-      print(event['input'].split(" ", 1)[0], "-" , man_pages_dict[event['input'].split(" ", 1)[0]])
-    except Exception:
-      print(event['input'], "- unknown command")
+  with open(man_pages) as f:
+    man_pages_dict = json.load(f)
+
+  for event in total_contents:
+    if 'cowrie.command.input' in event['eventid']:
+      try:
+        print(event['input'].split(" ", 1)[0], "-" , man_pages_dict[event['input'].split(" ", 1)[0]])
+      except Exception:
+        print(event['input'], "- unknown command")
+
+if __name__ == "__main__":
+    main()
