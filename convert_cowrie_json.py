c2bcd9e927b48b63a09d53d8d7d79801a88104a5
dmcalpineuk <daniel.mcalpine@startmail.com>
2024-11-25 19:02:25 +1300

    Refresh

--- convert_cowrie_json.py
+++ convert_cowrie_json.py
@@ -12,22 +12,27 @@ __dependecies__ = "os, json, SMET, sentence_transformers"
 
 import json
 
+#input file
 file_name = './cowrie_2020-02-29.json'
+#output file
 file_path = './cowrie.json'
 
 def main():
 
   string = ''
 
+  #read contents of input file
   with open(file_name, 'r') as openfile:
     total_contents = json.load(openfile)
 
+  #iterate through the events in input file and format to match standard cowrie output
   for event in total_contents:
     for item in event.values():
       for next in item:
         if 'cowrie.command.input' in next['eventid']:
            string += '{"eventid":"'+next['eventid']+'","input":'+json.dumps(next['message'].split(' ', 1)[1])+',"message":'+json.dumps(next['message'])+',"sensor":"'+next['sensor']+'","timestamp":"'+next['timestamp']+'","src_ip":"'+next['src_ip_identifier']+'","session":"'+next['session_id']+'"}\n'
 
+  #save output to file
   with open(file_path, 'w', newline='\n') as f:
     f.writelines(string)
     
--- main.py
+++ main.py
@@ -7,129 +7,182 @@ __license__ = "GPLv3"
 __version__ = "1.0.0"
 __maintainer__ = "Daniel McAlpine"
 __email__ = "contact@danielmcalpine.com"
-__status__ = "Development"
-__dependecies__ = "os, json, SMET, sentence_transformers"
+__status__ = "Prototype"
+__dependecies__ = "json, os, platform, re, subprocess, sentence_transformers, SMET"
 
-
-import os
-os.environ["TRANSFORMERS_VERBOSITY"] = "error"
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-import re
 import json
-from SMET import map_attack_vector
-import subprocess
+import os
 import platform
+import re
+import subprocess
 
+os.environ["TRANSFORMERS_VERBOSITY"] = "error"
 
-file_name = './cowrie.json'
+#from sentence_transformers import SentenceTransformer
+from SMET import map_text, map_attack_vector
+
+#input files and location
+cowrie_file = './cowrie.json'
 man_pages = './manpages.json'
-threshold = 0.02
-# Define the delimiters
-delimiters = r"[ |;\"]+"
 
+#output files and location
+commands_file = './commands.txt'
+
+#variables are adjustable to test outcome
+threshold = 0.02
+CVE = False
+
+# Define the delimiters
+primary_delimiters = r"[;&]+"
+secondary_delimiters = r"[;&|({\" ,\\/]+"
+
+def read_json_list(input_file):
+  #import json file as python list
+  contents = []
+  with open(input_file, 'r') as openfile:
+    for line in openfile:
+      contents.append(json.loads(line))
+  return contents
+
+def read_json_dict(input_file):
+  #import json file as python dictionary
+  with open(input_file) as openfile:
+    return json.load(openfile)
+
+def write_json_list(output_data, output_file):
+  #output list to json file with Unix newline
+  with open(output_file, 'w', newline='\n') as openfile:
+    openfile.write('\n'.join(output_data))
+
+def write_json_dict(output_data, output_file):
+  #output dictionary to json file with Unix newline
+  with open(output_file, 'w', newline='\n') as openfile:
+    for key in output_data.keys():
+      for line in output_data[key]:
+        if line is None:
+          openfile.write("\n")
+        else:
+          openfile.write(json.dumps(line.strip())+"\n")
+
+def get_command_description(command):
+  # Run the man command and capture the output
+  result = subprocess.run(
+    f"man {command} | col -b",
+    shell=True,
+    capture_output=True,
+    text=True,
+    check=True
+  )
+
+  # Split the output into lines
+  output = result.stdout.splitlines()
+
+  # Initialize variables
+  description_started = False
+  first_sentence = ""
+
+  for line in output:
+    if re.match(r'^\s*DESCRIPTION\s*$', line, re.IGNORECASE):
+      description_started = True
+      continue
+
+    if description_started:
+      # Collect lines until the end of the first sentence
+      first_sentence += line.strip(r'[.!?]') + " "
+      if re.search(r'[.!?]', line):  # Look for sentence-ending punctuation
+        break
+
+  # Return the first sentence, trimmed of excess whitespace
+  return first_sentence.strip()
+
+def map_commands(input):
+    
+  description_paragraph = ''
+
+  if platform.system() != "Linux":
+    #import man pages descriptions 
+    man_pages_dict = read_json_dict(man_pages)
+
+  #map each line on first word and create paragraph
+  for event in input:
+    if not event:
+      number = 0
+      mapping = map_text(description_paragraph, CVE = CVE)
+      print()
+      while number < len(mapping):
+        if mapping[number][1] > threshold:
+          print("- Mapping:", mapping[number][0])
+        number += 1
+      print()
+      description_paragraph = ''
+    else:
+      first_word = event.split()[0]
+      try:
+        if platform.system() == "Linux":
+          #generate command from native Linux man pages
+          description = get_command_description(first_word)
+        else:
+          description = man_pages_dict[first_word]
+        mapping = map_attack_vector(description)
+        print(first_word, ' - ', description, ' - ', mapping[0][0])
+        description_paragraph += description + ". "
+      except Exception as e:
+        #print(first_word, " - No description")
+        continue
 
 def main():
 
-  total_contents = []
+  #import cowrie.json data
+  total_contents = read_json_list(cowrie_file)
 
-  #import cowrie output from json file
-  with open(file_name) as openfile:
-    for line in openfile:
-      total_contents.append(json.loads(line))
+  print("Start")
+  print()
 
-  #import man pages short descriptions
-  with open(man_pages) as f:
-    man_pages_dict = json.load(f)
+  input = []
+  new_input = []
+  current_value = ""
 
-  dictionary = {}
-  count = 0
-
-  # group events by session
+  # create list of input events, in order, grouped by session
   for event in total_contents:
     if 'cowrie.command.input' in event['eventid']:
-      commands = [item.strip() for item in re.split(delimiters, event['input']) if item.strip()]
-      if event['session'] not in dictionary.keys():
-        dictionary[event['session']] = commands
-      else:
-        for command in commands:
-          dictionary[event['session']].append(command)
+      primary_commands = re.split(primary_delimiters, event['input'])
+      if not current_value:
+        current_value = event['session']
+      if current_value != event['session']:
+        input.append('')
+        #new_input.append('')
+      for command in primary_commands:
+        input.append(command.strip())
+      current_value = event['session']
 
-  with open('temp.txt', 'w') as f:
-    f.write(json.dumps(dictionary))
+  for event in total_contents:
+    if 'cowrie.command.input' in event['eventid']:
+      secondary_commands = re.split(secondary_delimiters, event['input'])
+      if not current_value:
+        current_value = event['session']
+      if current_value != event['session']:
+        input.append('')
+        #new_input.append('')
+      for command in secondary_commands:
+        input.append(command.strip())
+      current_value = event['session']
 
-  for item in dictionary.keys():
+  input.append('')
+  #new_input.append('')
 
-    print()
-    count += 1
-    print("Count:",count)
-    print('sessionID:', item)
-    print(dictionary[item])
-    print()
-    
-    string = ""
-    
-    for next in dictionary[item]:
-      description = []
-      try:
-        if platform.system() == "Linux":
-          # Run the man command and capture the output
-          result = subprocess.run(
-            f"man {next} | col -b",
-            shell=True,
-            capture_output=True,
-            text=True,
-            check=True
-          )
+  #create file containing list of commands
+  write_json_list(input, commands_file)
 
-          # Split the output into lines
-          output = result.stdout.splitlines()
+  #map_commands(input)
+  #map_commands(new_input)
 
-          # Initialize variables
-          description_started = False
-          first_sentence = ""
+  for each in input:
+    print(each)
+#  for each in new_input:
+#    print(each)
 
-          for line in output:
-            if re.match(r'^\s*DESCRIPTION\s*$', line, re.IGNORECASE):
-              description_started = True
-              continue
+  print("End")
 
-            if description_started:
-              # Collect lines until the end of the first sentence
-              first_sentence += line.strip(r'[.!?]') + " "
-              if re.search(r'[.!?]', line):  # Look for sentence-ending punctuation
-                break
-
-          # Return the first sentence, trimmed of excess whitespace
-          description =  first_sentence.strip()
-
-        else:
-          description = man_pages_dict[next]#.strip(r'[.]')
-
-        mapping = map_attack_vector(description)
-        print(next, "-" , description, "-", mapping[0])
-        string += description + ". "
-
-      except Exception:
-        continue
-        #print(next, "- unknown command")
-
-    print()
-    print()
-
-    print(string.strip())
-    mapping1 = map_attack_vector(string.strip())
-
-    print()
-    print()
-
-    number = 0
-    while number < len(mapping1):
-      if mapping1[number][1] > threshold:
-        print("- Mapping:", mapping1[number])
-      number += 1
-
-  print()
-  print()
 
 if __name__ == "__main__":
     main()
