885f2283da533a159a4de7e09506312de3c19917
dmcalpineuk <daniel.mcalpine@startmail.com>
2024-12-07 18:31:16 +1300

    Update

---
+++ convert_cowrie_json_Dev_Data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+__author__ = "Daniel McAlpine"
+__copyright__ = "Copyright 2024"
+__credits__ = "Junaid Haseeb, Vimal Kumar"
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Daniel McAlpine"
+__email__ = "contact@danielmcalpine.com"
+__status__ = "Development"
+__dependecies__ = "os, json, SMET, sentence_transformers"
+
+import json
+
+#input file
+file_name = './all_commands_from_log_files.json'
+#output file
+file_path = './cowrie.json'
+
+def main():
+
+  string = ''
+
+  #read contents of input file
+  with open(file_name, 'r') as openfile:
+    total_contents = json.load(openfile)
+
+  #iterate through the events in input file and format to match standard cowrie output
+  for event in total_contents:
+    for item in event.values():
+      for next in item:
+        if 'cowrie.command' in next['eventid']:
+           string += '{"eventid":"'+next['eventid']+'","input":'+json.dumps(next['message'].split(' ', 1)[1])+',"message":'+json.dumps(next['message'])+',"sensor":"'+next['sensor']+'","timestamp":"'+next['timestamp']+'","src_ip":"'+next['src_ip_identifier']+'","session":"'+next['session_id']+'"}\n'
+
+  #save output to file
+  with open(file_path, 'w', newline='\n') as f:
+    f.writelines(string)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ convert_cowrie_json_test_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+__author__ = "Daniel McAlpine"
+__copyright__ = "Copyright 2024"
+__credits__ = "Junaid Haseeb, Vimal Kumar"
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+__maintainer__ = "Daniel McAlpine"
+__email__ = "contact@danielmcalpine.com"
+__status__ = "Development"
+__dependecies__ = "os, json, SMET, sentence_transformers"
+
+import json
+
+#input file
+file_name = './all_commands_from_log_files.json'
+#output file
+file_path = './cowrie.json'
+
+def main():
+
+  string = ''
+
+  #read contents of input file
+  with open(file_name, 'r') as openfile:
+    total_contents = json.load(openfile)
+
+  #iterate through the events in input file and format to match standard cowrie output
+  for next in total_contents:
+    if 'cowrie.command' in next['eventid']:
+      string += '{"eventid":"'+next['eventid']+'","input":"'+next['input']+'","message":"'+next['message']+'","sensor":"'+next['sensor']+'","timestamp":"'+next['timestamp']+'","src_ip":"'+next['src_ip']+'","session":"'+next['session']+'"}\n'
+
+  #save output to file
+  with open(file_path, 'w', newline='\n') as f:
+    f.writelines(string)
+
+if __name__ == "__main__":
+    main()
